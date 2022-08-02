ee46e311b6bc7c7a60008af8905a23ce1e144027
Sakshi Shah <67853646+Sakshi-Shah5@users.noreply.github.com>
2022-08-02 09:05:58 +0530

    Create Whatsapp.java

---
+++ FactoryDesignPattern/NotificationSystem/Whatsapp.java
@@ -0,0 +1,8 @@
+package FactoryDesignPattern.NotificationSystem;
+
+public class Whatsapp implements Notification{
+
+	public void notifyUser() {
+	System.out.println("Sending Whatsapp notification");
+	}
+}
